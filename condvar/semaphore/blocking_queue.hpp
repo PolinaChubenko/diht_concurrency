90405384efc5dbf82c179d6e602c8f86773b2538
Polina Chubenko <>
2022-03-05 23:50:44 +0300

    atomicity control added

--- condvar/semaphore/blocking_queue.hpp
+++ condvar/semaphore/blocking_queue.hpp
@@ -11,23 +11,28 @@ namespace solutions {
 template <typename T>
 class BlockingQueue {
  public:
-  explicit BlockingQueue(size_t capacity) : capacity_(capacity), size_(0) {
+  explicit BlockingQueue(size_t capacity)
+      : capacity_(capacity), size_(0), control_atomicity_(1) {
   }
 
   // Inserts the specified element into this queue,
   // waiting if necessary for space to become available.
   void Put(T element) {
     capacity_.Acquire();
+    control_atomicity_.Acquire();
     size_.Release();
     deque_.emplace_back(std::move(element));
+    control_atomicity_.Release();
   }
 
   // Retrieves and removes the head of this queue,
   // waiting if necessary until an element becomes available
   T Take() {
     size_.Acquire();
+    control_atomicity_.Acquire();
     auto element = std::move(deque_[0]);
     deque_.pop_front();
+    control_atomicity_.Release();
     capacity_.Release();
     return element;
   }
@@ -35,6 +40,7 @@ class BlockingQueue {
  private:
   Semaphore capacity_;
   Semaphore size_;
+  Semaphore control_atomicity_;
   std::deque<T> deque_;
 };
 
