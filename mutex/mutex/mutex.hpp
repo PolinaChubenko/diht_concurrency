1ae29e34f9c96ba82074baa15126ac81665026a6
Polina Chubenko <>
2022-03-18 21:53:41 +0300

    counting contentions

--- mutex/mutex/mutex.hpp
+++ mutex/mutex/mutex.hpp
@@ -9,32 +9,22 @@ namespace stdlike {
 class Mutex {
  public:
   void Lock() {
-    uint32_t expected = 0;
-    if (!locked_.compare_exchange_strong(expected, 1)) {
-      // mutex was locked, we need to wait
-      // expected = 1 or 2
-      do {
-        if (expected != 2) {
-          locked_.compare_exchange_strong(expected, 2);
-        }
-        locked_.wait(2);
-        // try to lock again
-        // (we make locked_ = 2 as there may be another thread here)
-        expected = 0;
-      } while (!locked_.compare_exchange_strong(expected, 2));
+    count_contentions_.fetch_add(1);
+    while (is_locked_.exchange(1) == 1) {
+      is_locked_.FutexWait(1);
     }
   }
 
   void Unlock() {
-    if (locked_.fetch_sub(1) == 2) {
-      locked_.store(0);
-      locked_.notify_one();
+    is_locked_.store(0);
+    if (count_contentions_.fetch_sub(1) > 1) {
+      is_locked_.FutexWakeOne();
     }
   }
 
  private:
-  // 0 = is unlocked; 1 = is locked; 2 = is locked and has waiters
-  twist::stdlike::atomic<uint32_t> locked_{0};
+  twist::stdlike::atomic<uint32_t> is_locked_{0};
+  twist::stdlike::atomic<uint32_t> count_contentions_{0};
 };
 
 }  // namespace stdlike
