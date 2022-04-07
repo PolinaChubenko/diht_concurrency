c0724c0dfae5b9352b17775b22859475325f65e4
Polina Chubenko <>
2022-04-07 22:54:32 +0300

    while optimization

--- condvar/barrier/cyclic_barrier.hpp
+++ condvar/barrier/cyclic_barrier.hpp
@@ -19,44 +19,29 @@ namespace solutions {
 class CyclicBarrier {
  public:
   explicit CyclicBarrier(size_t participants) : participants_(participants) {
-    waiters_ = 0;
-    is_new_portion_ = false;
   }
 
   // Blocks until all participants have invoked Arrive()
   void Arrive() {
     std::unique_lock<twist::stdlike::mutex> unique_lock(mutex_);
-    // new threads need to wait for the barrier to process the previous batch
-    while (is_new_portion_) {
-      go_through_.wait(unique_lock);
-    }
-    // blocking as not all participants have invoked Arrive()
     if (++waiters_ < participants_) {
-      while (!is_new_portion_) {
+      size_t current_opening = opened_cnt_;
+      do {
         go_through_.wait(unique_lock);
-      }
-    }
-    // the last thread from this batch has arrived,
-    // we separate the portions of threads and wake up neighbors-participants
-    if (waiters_ == participants_) {
-      is_new_portion_ = true;
-      go_through_.notify_all();
-    }
-    // we count the number of released threads,
-    // when the last one leaves, we will allow a new batch of threads
-    // to move to the next stage in the barrier
-    if (--waiters_ == 0) {
-      is_new_portion_ = false;
+      } while (current_opening == opened_cnt_);
+    } else {
+      ++opened_cnt_;
+      waiters_ = 0;
       go_through_.notify_all();
     }
   }
 
  private:
+  twist::stdlike::mutex mutex_;
   twist::stdlike::condition_variable go_through_;
   size_t participants_;
-  size_t waiters_;
-  bool is_new_portion_;
-  twist::stdlike::mutex mutex_;
+  size_t waiters_ = 0;
+  size_t opened_cnt_ = 0;
 };
 
 }  // namespace solutions
