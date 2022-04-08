882a0499b3fc274ae4f7d3079910241a3c56e6e9
Polina Chubenko <>
2022-04-08 16:18:50 +0300

    one atomic

--- condvar/condvar/condvar.hpp
+++ condvar/condvar/condvar.hpp
@@ -13,28 +13,23 @@ class CondVar {
   template <class Mutex>
   void Wait(Mutex& mutex) {
     uint32_t notified = notified_.load();
-    changed_.store(notified);
-
     mutex.unlock();
     notified_.wait(notified);
     mutex.lock();
   }
 
   void NotifyOne() {
-    uint32_t changed = changed_.load();
-    notified_.store(changed + 1);
+    notified_.fetch_add(1);
     notified_.notify_one();
   }
 
   void NotifyAll() {
-    uint32_t changed = changed_.load();
-    notified_.store(changed + 1);
+    notified_.fetch_add(1);
     notified_.notify_all();
   }
 
  private:
   twist::stdlike::atomic<uint32_t> notified_{0};
-  twist::stdlike::atomic<uint32_t> changed_{0};
 };
 
 }  // namespace stdlike
